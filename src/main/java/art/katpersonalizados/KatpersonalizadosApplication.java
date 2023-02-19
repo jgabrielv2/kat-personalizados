2eef1242723df6884e5eab5191437446b14912f8
jgabrielv <jgabriel.vidal@outlook.com>
2023-02-19 00:09:23 -0300

    initial commit

---
+++ src/main/java/art/katpersonalizados/KatpersonalizadosApplication.java
@@ -0,0 +1,13 @@
+package art.katpersonalizados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatpersonalizadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatpersonalizadosApplication.class, args);
+	}
+
+}
