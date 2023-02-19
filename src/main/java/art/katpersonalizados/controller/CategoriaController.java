d1874652126381c4fb50efa007a2a8e424225473
jgabrielv <jgabriel.vidal@outlook.com>
2023-02-19 15:31:24 -0300

    Adicionado CategoriaController e ProdutoController

---
+++ src/main/java/art/katpersonalizados/controller/CategoriaController.java
@@ -0,0 +1,52 @@
+package art.katpersonalizados.controller;
+
+import art.katpersonalizados.dto.CategoriaDto;
+import art.katpersonalizados.model.Categoria;
+import art.katpersonalizados.service.CategoriaService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("categorias")
+public class CategoriaController {
+
+    private final CategoriaService categoriaService;
+
+    public CategoriaController(CategoriaService categoriaService){
+        this.categoriaService = categoriaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> salvar(@RequestBody @Valid CategoriaDto categoriaDto){
+        return categoriaService.salvar(categoriaDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> buscarPorId(@PathVariable Long id){
+        return categoriaService.buscarPorId(id);
+    }
+
+    @GetMapping("/{nome}")
+    public ResponseEntity<Categoria> buscarPorNome(@PathVariable String nome){
+        return categoriaService.buscarPorNome(nome);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> buscarTodos(){
+        return categoriaService.buscarTodos();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Categoria> atualizar (@PathVariable Long id, @RequestBody @Valid CategoriaDto categoriaDto){
+        return categoriaService.atualizar(id, categoriaDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id){
+        categoriaService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/art/katpersonalizados/controller/ProdutoController.java
@@ -0,0 +1,57 @@
+package art.katpersonalizados.controller;
+
+import art.katpersonalizados.dto.ProdutoDto;
+import art.katpersonalizados.model.Produto;
+import art.katpersonalizados.service.ProdutoService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService){
+        this.produtoService = produtoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> salvar(@RequestBody @Valid ProdutoDto produtoDto){
+        return produtoService.salvar(produtoDto);
+    }
+
+    @GetMapping("/descricao/{descricao}")
+    public ResponseEntity<List<Produto>> buscarPorDescricao(@PathVariable String descricao){
+        return produtoService.buscarPorDescricao(descricao);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> buscarPorId(@PathVariable Long id){
+        return produtoService.buscarPorId(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> buscarTodos(){
+        return produtoService.buscarTodos();
+    }
+
+    @GetMapping("/categoria/{nomeCategoria}")
+    public ResponseEntity<List<Produto>> buscarPorNomeDaCategoria(@PathVariable String nomeCategoria){
+        return produtoService.buscarPorNomeCategoria(nomeCategoria);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> atualizar(@PathVariable Long id, @RequestBody @Valid ProdutoDto produtoDto){
+        return produtoService.atualizar(id, produtoDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir (@PathVariable Long id){
+        produtoService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
+}
